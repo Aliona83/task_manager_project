af85a23c75ef19677674ecb5f6ca15573a3d3bc6
Aliona83 <alionajanusko@gmail.com>
2025-03-28 14:38:51 +0000

    Change TEMPLATES in settings.py

--- TaskManagerProject/settings.py
+++ TaskManagerProject/settings.py
@@ -56,9 +56,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'TaskManagerProject.urls'
-
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
