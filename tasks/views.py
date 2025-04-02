b77df145c3b972c3258ee4782660b3c6ecc0ab68
Aliona <alionajanusko@gmail.com>
2025-04-02 16:44:23 +0100

    Add filtering logic for views.py

--- tasks/views.py
+++ tasks/views.py
@@ -6,8 +6,26 @@ def task_list(request):
     tasks = Task.objects.all()
     categories = Category.objects.all()
     task_status = Task.STATUS_CHOICES
-    context = {'tasks':tasks,'categories':categories,'task_status':task_status}
-    return render(request,'tasks/tasks.html',context)
+
+    # Filtering logic
+    selected_category = request.GET.get('category')
+    selected_status = request.GET.get('status')
+
+    if selected_category:
+        tasks = tasks.filter(category_id=selected_category)
+
+    if selected_status:
+        tasks = tasks.filter(status=selected_status)
+
+    context = {
+        'tasks': tasks,
+        'categories': categories,
+        'task_status': task_status,
+        'selected_category': selected_category,
+        'selected_status': selected_status,
+    }
+
+    return render(request, 'tasks/tasks.html', context)
 
 def delete_task(request,pk):
     pass
