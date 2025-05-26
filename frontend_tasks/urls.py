57eeecc413dc5d087d48359c4618cda10ebc9e70
Aliona Janusko <111147735+Aliona83@users.noreply.github.com>
2025-05-26 11:19:11 +0100

    Fix erorr in task list view

--- frontend_tasks/urls.py
+++ frontend_tasks/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.i18n import urlpatterns
 from django.urls import path
-from .views import task_list,delete_task,register_view,login_view,logout_view,task_details,add_task
+from .views import task_list,delete_task,register_view,login_view,logout_view,task_details,add_task,weather_view
 
 urlpatterns = [
 path('register/', register_view, name='register'),
@@ -10,6 +10,7 @@ path('register/', register_view, name='register'),
     path('frontend_tasks/delete_task/<int:pk>/', delete_task, name='delete_task'),
     path('task_details/<int:pk>/', task_details, name='task_details'),
     path('add_task/', add_task, name='add_task'),
+    path('weather/', weather_view, name='weather'),
 
 
 
